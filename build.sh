147cdfd81df56e58338c8129f8c7c9e64aa01516
clafollette303 <lafollettec@etsu.edu>
2024-09-16 12:15:24 +0000

    changes to build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #Build docker images (BE SURE TO REPLACE <USERNAME> WITH YOUR OWN DOCKER USERNAME)
-docker build -f BucStop/Dockerfile -t <username>/bucstop .
-docker build -f GameMicroServer/Dockerfile -t <username>/gamemicro .
-docker build -f APIGateway/Dockerfile -t <username>/gamemicroapi .
-docker build -f GameMicroServer/Dockerfile -t <username>/gamemicrosnake .
-docker build -f GameMicroServer/Dockerfile -t <username>/gamemicropong .
-docker build -f GameMicroServer/Dockerfile -t <username>/gamemicrotetris .
+sudo docker build -f BucStop/Dockerfile -t clafollette321/bucstop .
+sudo docker build -f GameMicroServer/Dockerfile -t clafollette321/gamemicro .
+sudo docker build -f APIGateway/Dockerfile -t clafollette321/gamemicroapi .
+sudo docker build -f GameMicroServer/Dockerfile -t clafollette321/gamemicrosnake .
+sudo docker build -f GameMicroServer/Dockerfile -t clafollette321/gamemicropong .
+sudo docker build -f GameMicroServer/Dockerfile -t clafollette321/gamemicrotetris .
 
 #Push docker images (BE SURE TO REPLACE <USERNAME> WITH YOUR OWN DOCKER USERNAME)
-docker push <username>/bucstop
-docker push <username>/gamemicro
-docker push <username>/gamemicroapi
-docker push <username>/gamemicrosnake
-docker push <username>/gamemicropong
-docker push <username>/gamemicrotetris
+sudo docker push clafollette321/bucstop
+sudo docker push clafollette321/gamemicro
+sudo docker push clafollette321/gamemicroapi
+sudo docker push clafollette321/gamemicrosnake
+sudo docker push clafollette321/gamemicropong
+sudo docker push clafollette321/gamemicrotetris
